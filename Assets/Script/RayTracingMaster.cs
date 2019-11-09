55d2c8c1e19ce81246bd037055d65a10c17659dd
Aaronczww <785333692@qq.com>
2019-11-09 15:41:34 +0800

    First Day

---
+++ Assets/Script/RayTracingMaster.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayTracingMaster : MonoBehaviour
+{
+    public ComputeShader RayTracingShader;
+
+    private RenderTexture _target;
+
+    private Camera _camera;
+
+    private uint _currentSample = 0;
+
+    private Material _addMaterial;
+
+    public Texture skybox;
+
+    public Light DirectionalLight;
+
+    struct Sphere
+    {
+        public Vector3 position;
+        public float radius;
+        public Vector3 albedo;
+        public Vector3 specular;
+    }
+
+    public Vector2 SphereRadius = new Vector2(3.0f, 8.0f);
+    public uint SphereMax = 100;
+    public float SpherePlacementRadius = 100.0f;
+    private ComputeBuffer _sphereBuffer;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Render(destination);
+    }
+
+    private void OnEnable()
+    {
+        _currentSample = 0;
+        SetUpScene();
+    }
+
+    private void OnDisable()
+    {
+        if(_sphereBuffer != null)
+        {
+            _sphereBuffer.Release();
+        }
+    }
+
+    private void SetUpScene()
+    {
+        List<Sphere> spheres = new List<Sphere>();
+        // Add a number of random spheres
+        for (int i = 0; i < SphereMax; i++)
+        {
+            Sphere sphere = new Sphere();
+            sphere.radius = SphereRadius.x + Random.value * (SphereRadius.y - SphereRadius.x);
+            Vector2 randomPos = Random.insideUnitCircle * SpherePlacementRadius;
+            sphere.position = new Vector3(randomPos.x, sphere.radius, randomPos.y);
+
+            foreach (Sphere other in spheres)
+            {
+                float minDist = sphere.radius + other.radius;
+                if (Vector3.SqrMagnitude(sphere.position - other.position) < minDist * minDist)
+                    goto SkipSphere;
+            }
+
+            Color color = Random.ColorHSV();
+            bool metal = Random.value < 0.5f;
+            sphere.albedo = metal ? Vector3.zero : new Vector3(color.r, color.g, color.b);
+
+            sphere.specular = metal ? new Vector3(color.r, color.g, color.b) : Vector3.one * 0.04f;
+
+            spheres.Add(sphere);
+            SkipSphere:
+            continue;
+        }
+        _sphereBuffer = new ComputeBuffer(spheres.Count, 40);
+        _sphereBuffer.SetData(spheres);
+    }
+
+    private void Awake()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if(transform.hasChanged)
+        {
+            _currentSample = 0;
+            transform.hasChanged = false;
+        }
+    }
+
+    private void SetShaderParams()
+    {
+        RayTracingShader.SetMatrix("_CameraToWorld", _camera.cameraToWorldMatrix);
+        RayTracingShader.SetMatrix("_CameraInverseProjection", _camera.projectionMatrix.inverse);
+        Vector3 l = DirectionalLight.transform.forward;
+        RayTracingShader.SetVector("_DirectionalLight", new Vector4(l.x, l.y, l.z, DirectionalLight.intensity));
+        RayTracingShader.SetBuffer(0, "_Spheres", _sphereBuffer);
+    }
+
+    private void Render(RenderTexture destination)
+    {
+        InitRenderTexture();
+        SetShaderParams();
+
+        RayTracingShader.SetTexture(0, "_SkyboxTexture", skybox);
+
+        RayTracingShader.SetTexture(0, "Result",_target);
+        RayTracingShader.SetVector("_PixelOffset", new Vector2(Random.value, Random.value));
+        int threadGroupX = Mathf.CeilToInt(Screen.width / 8.0f);
+        int threadGroupY = Mathf.CeilToInt(Screen.height / 8.0f);
+        RayTracingShader.Dispatch(0, threadGroupX, threadGroupY, 1);
+
+        if (_addMaterial == null)
+        {
+            _addMaterial = new Material(Shader.Find("Unlit/AddShader"));
+        }
+        _addMaterial.SetFloat("_Sample", _currentSample);
+
+        Graphics.Blit(_target, destination,_addMaterial);
+        _currentSample++;
+    }
+
+    private void InitRenderTexture()
+    {
+        if(_target == null || _target.width != Screen.width || _target.height != Screen.height)
+        {
+            if(_target != null)
+            {
+                _target.Release();
+            }
+            _target = new RenderTexture(Screen.width, Screen.height, 0,RenderTextureFormat.ARGBFloat,
+                RenderTextureReadWrite.Linear);
+            _target.enableRandomWrite = true;
+            _target.Create();
+        }
+    }
+}
